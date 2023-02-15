36b81e8f1de8d8efcec7ea78573e96b3a7c75cd4
tyoc213 <506234+tyoc213@users.noreply.github.com>
2023-02-15 00:42:05 -0600

    infinite answer to life, the universe and everything

--- src/main.rs
+++ src/main.rs
@@ -127,13 +127,13 @@ impl Iterator for Empty {
     type Item=u32;
 
     fn next(&mut self) -> Option<Self::Item> {
-        None
+        Some(42)
     }
 }
 
 fn main() {
-    for i in Empty {
-        panic!("Wait, this shouldn't happen!");
+    for i in Empty.take(10) {
+        println!("The answer to life, the universe, and everything is {}", i);
     }
     println!("All done!");
 }
